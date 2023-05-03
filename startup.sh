9d09fc4c01b367b379bf63174dcafce5d03d26ee
heathlh <110844948+heathlh@users.noreply.github.com>
2023-05-02 23:13:00 -0400

    Delete startup.sh

--- startup.sh
+++
@@ -1,3 +0,0 @@
-export APIKEY="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"
-export MONGODB_URI="mongodb+srv://cs103aSpr23:oDyglzBvRw47EJAU@cluster0.kgugl.mongodb.net/?retryWrites=true&w=majority"
-npm start
